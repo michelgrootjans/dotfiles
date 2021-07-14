82d2e38cec72c3d7b0dfae84c674c60baa8f56f4
Michel Grootjans <mgrootjans@Michels-MacBook-Air.local>
2021-07-14 11:21:48 +0200

    split up files

--- install.sh
+++ install.sh
@@ -27,53 +27,16 @@ fi
 
 # Clone to ~/.dotfiles
 DOTFILES=$HOME/.dotfiles
+# DOTFILES="${DOTFILES:-$HOME/.dotfiles}"
+
 if [ ! -d $DOTFILES ]; then
   git clone https://github.com/michelgrootjans/dotfiles.git $DOTFILES
 fi
 
-
-# Install all our dependencies with bundle (See Brewfile)
-brew update
-brew tap homebrew/bundle
-brew bundle --file ~/.dotfiles/Brewfile
-brew cleanup
-
-# Check for Oh My Zsh and install if we don't have it
-if [ ! -d $HOME/.oh-my-zsh ]; then
-  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
-fi
-
-# Check for zsh-autosuggestions and install if we don't have it
-if [ ! -d $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]; then
-  git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-else
-  git -C $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions pull
-fi
-
-# Check for powerlevel10k and install if we don't have it
-if [ ! -d $HOME/.oh-my-zsh/custom/themes/powerlevel10k ]; then
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
-else
-  git -C $HOME/.oh-my-zsh/custom/themes/powerlevel10k pull
-fi
-
-# Install sdkman
-if ! command -v sdk &> /dev/null; then
-  curl -s "https://get.sdkman.io" | bash
-fi
-
-for file in .{zshrc,zprofile,gitconfig,gitignore,nvm-init,p10k.zsh}; do
-  # if file is symlink, delete it, else rename it to *.bak
-  [ -f $HOME/$file ] && mv $HOME/$file "$HOME/$file.bak"
-  ln -sf $DOTFILES/$file $HOME/$file
-done;
-unset file;
-
+source $DOTFILES/install/.software
+source $DOTFILES/install/.shell
+source $DOTFILES/install/.dotfiles
 source $DOTFILES/install/.customize
-
-# Install krisp
-# sudo softwareupdate --install-rosetta
-# brew install --cask krisp
 
 # Set macOS preferences - we will run this last because this will reload the shell
 source $DOTFILES/install/.macos
